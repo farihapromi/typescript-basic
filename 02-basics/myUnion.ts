c59036c5278c004a1f129798c6db9eff0bef8141
Fariha Promi <farihapromi2611@gmail.com>
2025-04-09 11:18:04 +0600

    Add union of typescript

---
+++ 02-basics/myUnion.ts
@@ -0,0 +1,14 @@
+let score:number|string
+score=44;
+score="56"
+
+type myUser={
+    name:string;
+    id:number
+}
+type Admin={
+    userName:string,
+    id:number
+}
+let promi:myUser|Admin={name:"fariha",id:293004}
+promi={userName:"rima",id:677}
